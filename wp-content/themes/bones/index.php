e325db38e7d5d50817dab100f4a874c7fe9aabf1
Troy Slaten <troy.slaten@laterisers.com>
2014-02-08 18:42:52 -0500

    Making sense of the included Sass files/added ACF to backend

--- wp-content/themes/bones/index.php
+++ wp-content/themes/bones/index.php
@@ -4,33 +4,33 @@
 
 				<div id="inner-content" class="wrap clearfix">
 
-						<div id="main" class="eightcol first clearfix" role="main">
+						<div id="main" class="twelvecol first clearfix" role="main">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+								<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
-								<header class="article-header">
+									<header class="article-header">
 
-									<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline vcard"><?php
-										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
+										<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+										<p class="byline vcard"><?php
+											printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
+										?></p>
 
-								</header>
+									</header>
 
-								<section class="entry-content clearfix">
-									<?php the_content(); ?>
-								</section>
+									<section class="entry-content clearfix">
+										<?php the_content(); ?>
+									</section>
 
-								<footer class="article-footer">
-									<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
+									<footer class="article-footer">
+										<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
 
-								</footer>
+									</footer>
 
-								<?php // comments_template(); // uncomment if you want to use them ?>
+									<?php // comments_template(); // uncomment if you want to use them ?>
 
-							</article>
+								</article>
 
 							<?php endwhile; ?>
 
@@ -62,8 +62,6 @@
 							<?php endif; ?>
 
 						</div>
-
-						<?php get_sidebar(); ?>
 
 				</div>
 
